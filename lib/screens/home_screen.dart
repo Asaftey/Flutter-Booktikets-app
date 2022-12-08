01ce03e7059840496c623b8e4c6d864dec48f6ef
Ionut Asaftei <ionut.asaftei2014@gmail.com>
2022-12-08 16:04:56 +0100

    seccond commit finished the main screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:booktickets/screens/hotel_screen.dart';
 import 'package:booktickets/screens/ticket_view.dart';
+import 'package:booktickets/utils/app_info_list.dart';
 import 'package:booktickets/utils/app_styles.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 
@@ -84,7 +86,47 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             const Gap(15),
-            TicketView(),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.only(left: 20),
+              child: Row(
+                children: [
+                  TicketView(),
+                  TicketView(),
+                  TicketView(),
+                ],
+              ),
+            ),
+            const Gap(15),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Hotels",
+                    style: Styles.headLineStyle2,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      print("you are taped");
+                    },
+                    child: Text("View all",
+                        style: Styles.textStyle
+                            .copyWith(color: Styles.primaryColor)),
+                  )
+                ],
+              ),
+            ),
+            const Gap(15),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.only(left: 20),
+              child: Row(
+                  children: hotelList
+                      .map((singleHotel) => Hotel(hotel: singleHotel))
+                      .toList()),
+            )
           ],
         ));
   }
--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -14,10 +14,10 @@ class TicketView extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = AppLayout.getSize(context);
     return SizedBox(
-      width: size.width,
+      width: size.width * 0.85,
       height: 200,
       child: Container(
-        margin: const EdgeInsets.only(left: 16),
+        margin: const EdgeInsets.only(right: 16),
         child: Column(
           children: [
             /*showing the blue part of the tiket*/
@@ -121,23 +121,27 @@ class TicketView extends StatelessWidget {
                     ),
                   ),
                   Expanded(
-                    child: LayoutBuilder(builder:
-                        (BuildContext context, BoxConstraints constraints) {
-                      return Flex(
-                        direction: Axis.horizontal,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        mainAxisSize: MainAxisSize.max,
-                        children: List.generate(
-                          (constraints.constrainWidth() / 15).floor(),
-                          ((index) => SizedBox(
-                              width: 5,
-                              height: 1,
-                              child: DecoratedBox(
-                                  decoration:
-                                      BoxDecoration(color: Colors.white)))),
-                        ),
-                      );
-                    }),
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 16, top: 10, right: 16, bottom: 16),
+                      child: LayoutBuilder(builder:
+                          (BuildContext context, BoxConstraints constraints) {
+                        return Flex(
+                          direction: Axis.horizontal,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize.max,
+                          children: List.generate(
+                            (constraints.constrainWidth() / 15).floor(),
+                            ((index) => SizedBox(
+                                width: 5,
+                                height: 1,
+                                child: DecoratedBox(
+                                    decoration:
+                                        BoxDecoration(color: Colors.white)))),
+                          ),
+                        );
+                      }),
+                    ),
                   ),
                   SizedBox(
                     height: 20,
@@ -153,6 +157,66 @@ class TicketView extends StatelessWidget {
                   ),
                 ],
               ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: Styles.orangeColor,
+                  borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(21),
+                      bottomRight: Radius.circular(21))),
+              padding: const EdgeInsets.all(16),
+              child: Column(children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "1 MAY",
+                          style: Styles.headLineStyle3
+                              .copyWith(color: Colors.white),
+                        ),
+                        Text(
+                          "Date",
+                          style: Styles.headLineStyle3
+                              .copyWith(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "08:00 AM",
+                          style: Styles.headLineStyle3
+                              .copyWith(color: Colors.white),
+                        ),
+                        Text(
+                          "Departure Time",
+                          style: Styles.headLineStyle3
+                              .copyWith(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          "23",
+                          style: Styles.headLineStyle3
+                              .copyWith(color: Colors.white),
+                        ),
+                        Text(
+                          "Number",
+                          style: Styles.headLineStyle3
+                              .copyWith(color: Colors.white),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ]),
             ),
           ],
         ),
